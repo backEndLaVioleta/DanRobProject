57632af92d6bbc93329fd45a964b8caf483c63a8
naranjito72 <theclick36@gmail.com>
2021-11-24 12:36:05 +0100

    swagger module added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,16 @@ async function bootstrap() {
 
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+  // config swagger
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Blossh Recipes')
+    .setDescription('Dan & Rob Api and MongoDB')
+    .setVersion('1.0')
+    .addTag('Hospitality App')
+    .build();
+
+  const docSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('test', app, docSwagger);
 
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
