be72ce64b25b4e7498c96b96ae5f064492d11a57
naranjito72 <theclick36@gmail.com>
2021-11-25 14:18:44 +0100

    eslint

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,7 +8,7 @@ export class User {
   @ObjectIdColumn() id: ObjectID;
   @Column() firstName: string;
   @Column() lastName: string;
-  @Column() email: string;
+  @Column({unique: true}) email: string;
   @Column() password: string;
   @Column() role: string;
   @Column() creation_date?: Date;
