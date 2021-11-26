cf50e247a5d06a3491fcb057e503935fe5d4e441
naranjito72 <theclick36@gmail.com>
2021-11-26 14:18:57 +0100

    dev-rob

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import UserAlreadyExistConstraint from 'src/common/validations/emailValidation';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, UserAlreadyExistConstraint],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
