cbb15fe5bc0f3b1674d426e118f0df003e6d3e20
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-11-26 12:55:38 +0100

    :art: Working version

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,19 +1,12 @@
-import { IsEmail } from 'class-validator';
-import { UserAlreadyExist } from 'src/common/validations/emailValidation';
-import { Column, Entity, ObjectID, ObjectIdColumn, Unique } from 'typeorm';
+import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
 
 @Entity()
-@Unique(['email'])
+
 export class User {
-  // to discuss with Dan
-  // @PrimaryGeneratedColumn() id: number
   @ObjectIdColumn() id: ObjectID;
   @Column() firstName: string;
   @Column() lastName: string;
-  @Column({ unique: true })
-  @IsEmail()
-  @UserAlreadyExist()
-  email: string;
+  @Column() email: string;
   @Column() password: string;
   @Column() role: string;
   @Column() creation_date?: Date;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,7 +5,7 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  Delete
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
