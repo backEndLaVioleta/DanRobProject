f0ab73bbb6dbc95eab5cf7c55f059d2b64b1bd71
naranjito72 <theclick36@gmail.com>
2021-12-08 12:53:53 +0100

    auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import LoginUserDto from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<string | any> {
+    console.log(email, pass);
+    const user = await this.userService.findOneByEmail(email);
+    console.log(user);
+    let myResponse: any;
+    user ? (myResponse = user) : (myResponse = null);
+    console.log(myResponse);
+    return myResponse;
+  }
+
+  async login(user: LoginUserDto) {
+    console.log(user);
+    const payload = { email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
