e4a584c65e95dc18c3c2bf541186416c1b9379f6
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-12-09 14:21:33 +0100

    :poop: adminUser decorator intent

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,7 @@ import { UserDto } from './dto/user.dto';
 import { AuthService } from 'src/auth/auth.service';
 import { LoginUserDto } from 'src/auth/dto/login.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { AdminUser } from 'src/interceptors/adminUser.interceptor';
 
 @Controller('users')
 @Serialize(UserDto)
@@ -49,6 +50,12 @@ export class UsersController {
       findAllUsersDto.limit,
     );
   } */
+
+  @Get('/whoami')
+  whoami(@AdminUser() user: string) {
+    console.log('hello');
+    return user;
+  }
 
   @Get('/:id')
   async findOneById(@Param('id') id: string) {
